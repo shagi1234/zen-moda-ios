7a11aa459a8e3fc0528b80833ba3bdfbeb8ed585
shagi1234 <mobdeveloper1409@gmail.com>
2025-06-07 13:00:21 +0500

    Initial Commit

---
+++ ZenModaIOS/ZenModaIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenModaIOSApp.swift
+//  ZenModaIOS
+//
+//  Created by Shahruh on 07.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct ZenModaIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
